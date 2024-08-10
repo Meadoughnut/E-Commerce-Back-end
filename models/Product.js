c248d7e2fdacf93af4c9ec835d100d0332b05cab
Meadoughnut <127433011+Meadoughnut@users.noreply.github.com>
2024-08-10 11:43:50 -0400

    Updates all the model

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const { types } = require('pg');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,7 +10,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    location_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      types: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    catagoty_id: {
+      type: DataTypes.INTEGER,
+      refernces:{
+        model: 'Category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
@@ -21,3 +53,8 @@ Product.init(
 );
 
 module.exports = Product;
+
+
+
+
+      
